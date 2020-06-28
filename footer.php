b9f9bea19284bc994a14c9028eca5b727dd4ee27
MadDog-420 <u18211928@utp.edu.pe>
2020-06-28 01:02:29 -0500

    exerciseON

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 
-    <footer id="footer" class="pb-5 pt-4">
-        <div class="container">
-            <div class="row text-center navbar-light">
+    <footer id="footer" class="pb-5 pt-4 navbar-dark">
+        <div class="container navbar-nav">
+            <div class="row text-center">
                 <div class="col-12 col-lg"><a type="button" class="nav-link" href="" data-toggle="modal" data-target="#contact-modal">Contáctanos</a></div>
                 <div class="col-12 col-lg"><a class="nav-link disabled" href="#">Términos y condiciones</a></div>
                 <div class="col-12 col-lg"><a class="nav-link disabled" href="#">Privacidad</a></div>
--- index.php
+++ index.php
@@ -11,13 +11,14 @@
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <!-- FontAwesome -->
         <script src="https://kit.fontawesome.com/a7b3ea38a5.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css2?family=MuseoModerno&family=Work+Sans:wght@400;700&display=swap" rel="stylesheet">
         <!-- Optional JavaScript -->
         <script src="JS/menu.js"></script>
     </head>
     <body>
         <?php require_once "header.php";?>
-        <main>
-            
+        <main style="padding: 150px 10% 50px 10%">
+            <?php require_once "resolution.php";?>
         </main>
         <?php require_once "footer.php";?>
     </body>
