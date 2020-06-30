b1c01473f73e2961080643312afd450d22bad9f5
MadDog-420 <u18211928@utp.edu.pe>
2020-06-30 11:31:35 -0500

    exOn

---
+++ ejercicio2.php
@@ -0,0 +1,34 @@
+<?php
+
+    ejercicio1(3);
+    ejercicio2(5,4);
+    echo ejercicio3(5);
+    
+    function ejercicio1($n){
+        $mod=$n%2;
+        echo "$n es ";
+        if($mod == 0){
+            echo "par";
+        } else {
+            echo "impar";
+        }
+        echo "<br/><br/>";
+    }
+    
+    function ejercicio2($n1,$n2){
+        if($n1 > $n2){
+            echo "$n1 es mayor";
+        } else {
+            if($n1 < $n2){
+                echo "$n2 es mayor";
+            } else {
+                echo "$n1 y $n2 son iguales";
+            }
+        }
+        echo "<br/><br/>";
+    }
+
+    function ejercicio3($num){
+        return pow($num,2);
+    }
+?>
